065a3418d36db9858dc2fb5f65b9bd697f5176e5
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-10 12:35:19 +0500

    Update IntegerDivider.java

--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -1,8 +1,19 @@
 package com.epam.conditions;
 
 public class IntegerDivider {
-
     public void printCompletelyDivided(int dividend, int divider) {
-    }
+        if (divider == 0) {
+            System.out.println("Division by zero");
+            return;
+        }
 
+        int result = dividend / divider;
+        int product = result * divider;
+
+        if (product == dividend) {
+            System.out.println("Can be divided completely");
+        } else {
+            System.out.println("Cannot be divided completely");
+        }
+    }
 }
