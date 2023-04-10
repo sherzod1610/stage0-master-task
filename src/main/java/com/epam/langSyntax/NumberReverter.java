26ec540dcd401fd13ac3db51fde6c8f195aa0221
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-10 12:08:38 +0500

    Update NumberReverter.java

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,11 @@
 package com.epam.langSyntax;
 
 public class NumberReverter {
-
     public void revert(int number) {
+        int firstDigit = number % 10;
+        int secondDigit = (number / 10) % 10;
+        int thirdDigit = number / 100;
+        int reversedNumber = firstDigit * 100 + secondDigit * 10 + thirdDigit;
+        System.out.println(reversedNumber);
     }
-
 }
