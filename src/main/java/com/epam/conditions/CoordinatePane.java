43ed44e8d59a2baf05f801349e9e9e37b528693e
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-10 12:41:51 +0500

    Update CoordinatePane.java

--- src/main/java/com/epam/conditions/CoordinatePane.java
+++ src/main/java/com/epam/conditions/CoordinatePane.java
@@ -1,9 +1,18 @@
 package com.epam.conditions;
 
-public class CoordinatePane {
-
-    public void printQuadrant(int x, int y) {
+public class CoordinatePlane {
+    public void printQuadrant (int x, int y) {
+        if (x > 0 && y > 0) {
+            System.out.println("First quadrant ");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Second quadrant ");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Third quadrant");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Fourth quadrant");
+        } else {
+            System.out.println("The point is at the origin (0,0)");
+        }
     }
-
 }
 
