1ca900a8fc619432e102f4c0d7bb17cea713c29c
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-10 13:08:12 +0500

    Update HalfPyramid.java

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,7 +3,15 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k > 0; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
