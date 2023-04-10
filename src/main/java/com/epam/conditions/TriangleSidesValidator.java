3110c5fd5915f6f44932d6a11628a989a2b0fccd
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-10 12:22:30 +0500

    Update TriangleSidesValidator.java

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -1,9 +1,15 @@
 package com.epam.conditions;
 
-public class TriangleSidesValidator {
-
+public class TriangleValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0) {
+            System.out.println("It's not a triangle");
+            return;
+        }
+        if ((firstSide + secondSide > thirdSide) && (secondSide + thirdSide > firstSide) && (thirdSide + firstSide > secondSide)) {
+            System.out.println("This is a valid triangle");
+        } else {
+            System.out.println("It's not a triangle");
+        }
     }
-
 }
