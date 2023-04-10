b3110de6d499616f77ab2dc5c65f65b5b0e36bd7
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-10 12:27:33 +0500

    Update DaysInMonth.java

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,37 @@
 package com.epam.conditions;
 
 public class DaysInMonth {
-
     public void printDays(int year, int month) {
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("Invalid date");
+            return;
+        }
 
+        int days = 0;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                days = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = 30;
+                break;
+            case 2:
+                if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                    days = 29;
+                } else {
+                    days = 28;
+                }
+                break;
+        }
+        System.out.println(days);
     }
-
 }
