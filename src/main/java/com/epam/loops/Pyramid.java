5baadb91ae5641e2db3fff596ffa115a917ae270
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-10 13:01:19 +0500

    Update Pyramid.java

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -1,13 +1,22 @@
 package com.epam.loops;
 
 public class Pyramid {
-
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k > 0; k--) {
+                System.out.print(k);
+            }
+            for (int k = 2; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
-
 }
