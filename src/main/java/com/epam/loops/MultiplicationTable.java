8c52679db22e68a64fc88b58f4d269cf124bd262
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-10 12:49:08 +0500

    Update MultiplicationTable.java

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -1,8 +1,25 @@
 package com.epam.loops;
+import java.util.Scanner;
 
 public class MultiplicationTable {
-
     public void printTable(int numberTableToPrint) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numberTableToPrint + " x " + i + " = " + (numberTableToPrint * i));
+        }
     }
-
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        
+        if (number <= 0) {
+            System.out.println("Invalid input.");
+            return;
+        }
+        
+        MultiplicationTable table = new MultiplicationTable();
+        table.printTable(number);
+    }
 }
