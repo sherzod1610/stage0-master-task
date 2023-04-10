994a09712da1ff8ee935ddbf48677e826e3ff6ed
sherzod1610 <108582436+sherzod1610@users.noreply.github.com>
2023-04-10 12:00:51 +0500

    Update Animal.java

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -16,4 +16,13 @@ public class Animal {
         String pawWord = numberOfPaws == 1 ? "paw" : "paws";
         return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + pawWord + " and " + fur + " fur.";
     }
+    
+    public void optionalTask(){
+        Dog myDog = new Dog();
+        System.out.println(myDog.getDescription()); // "This animal is mostly brown. It has 4 paws and a fur."
+
+        Bird myBird = new Bird();
+        System.out.println(myBird.getDescription()); // "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly."
+    }
+    
 }
